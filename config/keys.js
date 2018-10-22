c29b3a22e1803edf8b15abe550b65d1eb392656b
unknown <brcampbell001@outlook.com>
2018-10-21 20:05:22 -0500

    JWT created

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://webenjammin:webenjammin1234@ds137703.mlab.com:37703/socialnetwork"
+    "mongodb://webenjammin:webenjammin1234@ds137703.mlab.com:37703/socialnetwork",
+  secretOrKey: "secret"
 };
