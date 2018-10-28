fae783dbc6296a6eb52613b7d4fef19d9ccfa232
unknown <brcampbell001@outlook.com>
2018-10-28 09:26:47 -0500

    Get all posts and post by id routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,6 +15,29 @@ const validatePostInput = require("../../validation/post");
 
 router.get("/test", (req, res) => res.json({ msg: "Posts working" }));
 
+// @route  GET api/posts
+// @desc   Get Posts
+// @access Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({ nopostsfound: "No posts were found" })
+    );
+});
+
+// @route  GET api/posts/:id
+// @desc   Get post by id
+// @access Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that id" })
+    );
+});
+
 // @route  POST api/posts
 // @desc   Create Post
 // @access Private
